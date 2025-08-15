0453a237bcae3dd9aec82ad3f20a46c4282070cf
karpix25 <nadaraya@gmail.com>
2025-08-15 17:43:56 -0300

    Update index.ts

--- frontend/src/components/layout/index.ts
+++ frontend/src/components/layout/index.ts
@@ -1,5 +1,1 @@
-// Layout Components exports
-export { Header } from './header'
-export { Footer } from './footer'
-export { Sidebar } from './sidebar'
-// Add more exports as you create components
+
