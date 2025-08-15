61eee66a71d7b4c925c9b4ede8fddacb5910d0bc
karpix25 <nadaraya@gmail.com>
2025-08-15 16:57:15 -0300

    Update index.ts
    базовый layouint

--- frontend/src/components/layout/index.ts
+++ frontend/src/components/layout/index.ts
@@ -1,1 +1,5 @@
-
+// Layout Components exports
+export { Header } from './header'
+export { Footer } from './footer'
+export { Sidebar } from './sidebar'
+// Add more exports as you create components
